34348b276c8687086c027e94a81fabac6c875a40
Muskaan <connect.tomuskaan@gmail.com>
2023-07-16 01:28:39 +0530

    Fixed tailwind

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -190,7 +190,10 @@ export default function Example() {
         <div className="py-10">
           <header>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Dashboard</h1>
+             <div className="border-b border-gray-200 pb-5">
+                 <h3 className="text-base font-semibold leading-6 text-left text-gray-900">AI News Generator</h3>
+                 <p className="mt-2 max-w-4xl text-left text-sm text-gray-500">Enter BBC article link below</p>
+    </div>
             </div>
           </header>
           <main>
